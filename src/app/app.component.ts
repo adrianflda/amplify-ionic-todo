9a1dbf5197a5877b00c8a0d98ff5e664b5ea2c50
adrian moreno perez <adrian.flda@gmail.com>
2020-07-14 17:35:52 -0400

    autentication form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,68 @@
-import { Component } from '@angular/core';
-
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Component } from "@angular/core";
+import { Platform } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
+import { APIService } from "./API.service";
+import { FormFieldTypes } from "@aws-amplify/ui-components";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
+  todos: Array<any>;
+  formFields: FormFieldTypes;
+  authenticated: Boolean;
+  user: Object;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private apiService: APIService
   ) {
+    this.formFields = [
+      {
+        type: "email",
+        label: "Custom email Label",
+        placeholder: "custom email placeholder",
+        required: true,
+      },
+      {
+        type: "password",
+        label: "Custom Password Label",
+        placeholder: "custom password placeholder",
+        required: true,
+      },
+      {
+        type: "phone_number",
+        label: "Custom Phone Label",
+        placeholder: "custom Phone placeholder",
+        required: false,
+      },
+    ];
     this.initializeApp();
+  }
+
+  createTodo() {
+    this.apiService.CreateTodo({
+      name: "ionic",
+      description: "testing",
+    });
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.apiService.ListTodos().then((evt) => {
+        this.todos = evt.items;
+      });
+      this.apiService.OnCreateTodoListener.subscribe((evt) => {
+        const data = (evt as any).value.data.onCreateTodo;
+        this.todos = [...this.todos, data];
+      });
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouteReuseStrategy } from "@angular/router";
 
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { IonicModule, IonicRouteStrategy } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+
+import { AmplifyUIAngularModule } from "@aws-amplify/ui-angular";
+import Amplify from "aws-amplify";
+import awsconfig from "../aws-exports";
+
+Amplify.configure(awsconfig);
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    AmplifyUIAngularModule,
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+  ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
